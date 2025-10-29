328c667a42111cd0b7485543bfb34129a2292f7b
Christer Dale Reyes <rchristerdale@gmail.com>
2025-10-30 01:42:42 +0800

    chore: update frontend dependencies and add semantic release configuration
    - Added "@react-native/new-app-screen" to dependencies.
    - Removed duplicate entry of "@react-native/new-app-screen".
    - Added semantic release related packages to devDependencies.
    - Created a new release configuration file for semantic release.

---
+++ frontend/release.config.js
@@ -0,0 +1,36 @@
+export default {
+  branches: ["main"],
+  repositoryUrl: "https://github.com/Montilla007/Trailblazer.git",
+  tagFormat: "frontend-v${version}",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "feat", release: "minor", scope: "frontend" },
+          { type: "fix", release: "patch", scope: "frontend" },
+          { type: "perf", release: "patch", scope: "frontend" },
+          { type: "refactor", release: "patch", scope: "frontend" },
+          { type: "revert", release: "patch", scope: "frontend" },
+          { type: "chore", release: false },
+          { type: "docs", release: false },
+          { type: "style", release: false },
+          { type: "test", release: false },
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        },
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["frontend/package.json", "frontend/CHANGELOG.md"],
+        message: "chore(release-frontend): ${nextRelease.version} [skip ci]",
+      },
+    ],
+  ],
+};
